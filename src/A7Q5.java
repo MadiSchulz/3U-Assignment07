b1be2d52361388eeb68358ae20ba67652267f154
madis <madis@DESKTOP-MA781AP>
2016-12-16 10:39:00 -0500

    12/16. Finished Q1 to 5.

--- src/A7Q5.java
+++ src/A7Q5.java
@@ -22,7 +22,6 @@ public class A7Q5 {
         System.out.println("Number of marks:");
         // Store # of marks
         int numMarks = input.nextInt();
-        
         // Create array of marks
         int[] marks = new int[numMarks];
         // Loop to populate array
@@ -32,7 +31,6 @@ public class A7Q5 {
             // Fill spot
             marks[i] = input.nextInt();
         }
-        
         // Loop for each spot in array
         for (int i = 0; i < marks.length; i++) {
             // Loop for the number of spots after i 
@@ -54,9 +52,24 @@ public class A7Q5 {
         for (int i = 0; i < marks.length; i++) {
             System.out.println(marks[i] + "  ");
         }
-        
-        // Determine if the length of the array is an even or odd number
-        // Divide array length into groups of two (A6Q9)
-            
+        // Variable for length of array
+        int length = marks.length;
+        // If length is even
+        if (length % 2 == 0) {
+            // Get the two middle numbers
+            int upperMid = marks[length / 2];
+            int lowerMid = marks[(length / 2) - 1];
+            // Add numbers and divide total to get media
+            int media = (upperMid + lowerMid) / 2;
+            // Output the media
+            System.out.println("The media is " + media);
+        }
+        // If length is odd
+        if (length % 2 == 1) {
+            // Get the middle number
+            int median = marks[length / 2];
+            // Output the median
+            System.out.println("The median is " + median);
+        }
     }
 }
